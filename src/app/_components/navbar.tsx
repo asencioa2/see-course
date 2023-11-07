8da400c56a17f3559c150f97afa2b53eb54d7d6b
Allen Asencio <allenasencio@gmail.com>
2023-11-07 13:36:01 -0500

    Fixed Img Update

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar(){
         <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="#" className="flex items-center">
-                <Image src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
+                <Image src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" height={40} width={40}/>
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SEE</span>
             </a>
             <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ export default function Page() {
         <section className="flex">
             <div className="w-2/3 mx-12">
                 <p className="text-center text-xl m-4" >Welcome to the Software Engineering Ethics Course</p>
-                <Image src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="" />
+                <Image src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="" width={1000} height={800} />
                 <p className="text-center text-lg" >This Software Engineering Ethics course explores ethical considerations and responsibilities in the development, deployment, and maintenance of software applications. Topics include privacy, security, intellectual property, social impact, and professional conduct. Students learn to navigate moral dilemmas and make informed decisions in the field of software engineering.</p>
             </div>
             <div className="w-1/3 mx-12 ">
                 <h1 className="text-center text-xl m-4">Announcements</h1>
                 <Announcement title="Exam #1 Reminder:" desc="Please take Exam 1 before researching the provided lesson plans!"/>
-                <Announcement title="Exam #1 Reminder:" desc="After you have taken Exam 2 if you time please take the Questionnaire"/>
+                <Announcement title="Website Questionnaire:" desc="After you have taken Exam 2 if you time please take the Questionnaire"/>
             </div>
         </section>
     )
