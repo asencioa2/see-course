9916d4f8912a8c4b3dc1ce48de8b902660c20ca2
Allen Asencio <allenasencio@gmail.com>
2023-11-07 13:17:52 -0500

    Updated nxt Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Announcement from "./_components/announcement"
+import Image from "next/image"
 
 export default function Page() {
     return (
         <section className="flex">
             <div className="w-2/3 mx-12">
                 <p className="text-center text-xl m-4" >Welcome to the Software Engineering Ethics Course</p>
-                <img src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="" />
+                <Image src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="" />
                 <p className="text-center text-lg" >This Software Engineering Ethics course explores ethical considerations and responsibilities in the development, deployment, and maintenance of software applications. Topics include privacy, security, intellectual property, social impact, and professional conduct. Students learn to navigate moral dilemmas and make informed decisions in the field of software engineering.</p>
             </div>
             <div className="w-1/3 mx-12 ">
